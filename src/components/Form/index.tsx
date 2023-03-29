98591497fd3bb4926d881a5f335e2d7c15a2b7ce
Ruan Finamor <ruanfinamor@gmail.com>
2023-03-29 16:43:08 -0300

    refactor: centralizando selects, instalando Icons

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,38 +1,84 @@
-import { Box, Button, TextField, MenuItem, Card, CardContent, Grid } from "@mui/material";
+import {  Button, TextField, MenuItem, Card, CardContent, Grid } from "@mui/material";
 import React, { useState } from "react";
+import SendIcon from '@mui/icons-material/Send';
 
 function Form() {
   const [marcas, setMarcas] = useState<string>("1");
+  const [modelos, setModelos] = useState<string>("1");
+  const [ano, setAno] = useState<string>("1");
 
   return (
     <Card>
       <CardContent>
-        <Grid container spacing={1}>
-          <Grid xs={12} item>
-            <TextField
+
+        <Grid xs={12} item>
+          
+        </Grid>
+
+        <Grid container spacing={4}>
+          <Grid xs={12}  item>
+            <TextField             
               label="Selecionar marca"
               select
+              variant="outlined"
               value={marcas}
+              sx={{width: {sm: 300}}}
               onChange={(evento) => {
                 setMarcas(evento.target.value);
               }}
-            />
-            <MenuItem value="1" selected disabled>
-            Marca do Carro
-            </MenuItem>
-            <MenuItem value="fiat">Fiat</MenuItem>
-            <MenuItem value="ford">Ford</MenuItem>
-          </Grid>
-          
+            >
+              <MenuItem value="1" selected disabled>
+                Marca do Carro
+              </MenuItem>
+              <MenuItem value="fiat">Fiat</MenuItem>
+              <MenuItem value="ford">Ford</MenuItem>
+            </TextField>
 
-          
+
+          </Grid>
+
           <Grid xs={12} item>
-            <Button>Enviar</Button>
+            <TextField
+              label="Selecionar modelo"
+              select
+              value={modelos}
+              sx={{width: {sm: 300}}}
+              onChange={(evento) => {
+                setModelos(evento.target.value);
+              }}
+            >
+              <MenuItem value="1" selected disabled>
+                Modelo do Carro
+              </MenuItem>
+              <MenuItem value="celta">Celta</MenuItem>
+              <MenuItem value="ranger">Ranger</MenuItem>
+            </TextField>
           </Grid>
-        
-      </Grid>
+          <Grid xs={12} item>
+            <TextField
+              label="Selecionar ano"
+              select
+              value={ano}
+              sx={{width: {sm: 300}}}
+              onChange={(evento) => {
+                setAno(evento.target.value);
+              }}
+            >
+              <MenuItem value="1" selected disabled>
+                Ano do Carro
+              </MenuItem>
+              <MenuItem value="1999">1999</MenuItem>
+              <MenuItem value="2001">2001</MenuItem>
+            </TextField>
+          </Grid>
 
-    </CardContent>
+          <Grid xs={12} item>
+            <Button variant="contained" endIcon={<SendIcon />}>Enviar</Button>
+          </Grid>
+
+        </Grid>
+
+      </CardContent>
 
     </Card >
   );
